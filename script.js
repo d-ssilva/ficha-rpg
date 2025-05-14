628b19e46140099e2749dc1df3ee644221a87f39
danilo.silva <danilo.dos.s.silva@gmail.com>
2025-05-14 18:08:34 -0300

    Lógica de atributos/ modificadores

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-
 function calculoMod() {
-    let v = document.querySelectorAll('.atributo input').value
-    v.forEach(element => {
-        alert(element)        
-    });
+    let atributo = document.querySelectorAll('.atributo-valor')
+
+    atributo.forEach(element => {
+        let span = element.nextElementSibling
+
+        let valor = parseInt(element.value) || 0
+
+        let mod = Math.round(valor - 10) / 2
+
+        span.innerHTML = mod >= 0 ? `+${mod}` : mod
+    })
 }
+
+//Essa função esta verificando se algo esta sendo digitado nos inputs da classe atributo-valor
+document.querySelectorAll('.atributo-valor').forEach(input => {
+    input.addEventListener('input', calculoMod)
+
+})
+
